abc047ed71ef34f4e8ebfe6cc42a04e0b7b69f6f
Minouspi <yuna.gilbert@efrei.net>
2026-04-08 10:22:49 +0200

    Backend with authentication version

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,12 @@
-from flask import Flask
+from flask import Flask, request, abort
 
 app = Flask(__name__)
 
 @app.route("/data")
 def data():
+    token = request.headers.get("Authorization")
+    if token != "Bearer SECRET123":
+        abort(403)
     return "Sensitive data"
 
 app.run(host="0.0.0.0", port=5000)
